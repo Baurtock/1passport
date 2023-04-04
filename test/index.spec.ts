f0bf60a4ec4db34056c163695bbb5f540b747a9c
baurtock <xabierrena@gmail.com>
2023-04-04 20:55:07 +0200

    feature: Init project

---
+++ test/index.spec.ts
@@ -0,0 +1,11 @@
+import { index } from '../src/index';
+
+import { expect } from 'chai';
+
+describe("test", () => {
+    it("should return true", () => {
+        expect(index('test')).to.equal(true);
+    });
+});
+
+
