a889e6fa7037eef5165f74ba944e66fac8d4cc8f
baurtock <xabierrena@gmail.com>
2023-06-24 17:57:46 +0200

    fix: module exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { OnePassport } from 'core/OnePassport';
 import { Providers } from 'definitions/enums/Providers';
 import { Provider } from 'definitions/interfaces/Provider';
@@ -5,5 +6,12 @@ import { Provider } from 'definitions/interfaces/Provider';
 export {
     OnePassport,
     Provider,
+=======
+import { OnePassport } from './core/OnePassport';
+import { Providers } from './definitions/enums/Providers';
+
+module.exports = {
+    OnePassport,
+>>>>>>> Stashed changes
     Providers
 };
